5229c076eccb3eb850c37394f3fdf23451e51ee5
jamangueira7 <jamjs7@gmail.com>
2018-10-19 13:41:15 -0300

    first commit

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Client;
+use Illuminate\Http\Request;
+
+class ClientsController extends Controller
+{
+    public function index()
+    {
+        return Client::all();
+    }
+    public function show($id)
+    {
+        return Client::find($id);
+    }
+
+    public function store(Request $request)
+    {
+        $client = Client::create($request->all());
+        return response()->json($client, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $client = Client::find($id);
+        $client->fill($request->all());
+        $client->save();
+        return response()->json([], 204);
+    }
+}
